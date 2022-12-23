7e66437eeb1b1875fa3b7bae5d5d4f82d64e3e15
goldenchest19 <kirya-ignatev-2016@mail.ru>
2022-12-23 15:51:49 +0300

    upgrade task 5

--- src/Task5.java
+++ src/Task5.java
@@ -5,9 +5,10 @@ public class Task5 {
     public static void main(String[] args) {
 
         System.out.println("task 1");
-        System.out.println(encrypt("Hello"));
-        System.out.println(encrypt("Sunshine"));
+        System.out.println(Arrays.toString(encrypt("Hello")));
+        System.out.println(Arrays.toString(encrypt("Sunshine")));
         System.out.println(decrypt(72, 33, -73, 84, -12, -3, 13, -13, -68));
+        System.out.println(decrypt(encrypt("Hello")));
         System.out.println();
 
         System.out.println("task 2");
@@ -17,7 +18,7 @@ public class Task5 {
         System.out.println();
 
         System.out.println("task 3");
-        System.out.println(canComplete("butl", "beautifulqq"));
+        System.out.println(canComplete("butl", "beautiful"));
         System.out.println(canComplete("butlz", "beautiful"));
         System.out.println(canComplete("tulb", "beautiful"));
         System.out.println(canComplete("bbutl", "beautiful"));
@@ -43,12 +44,12 @@ public class Task5 {
         System.out.println("task 7");
         System.out.println(numToEng(0));
         System.out.println(numToEng(18));
-        System.out.println(numToEng(126));
-        System.out.println(numToEng(909));
+        System.out.println(numToEng(134));
+        System.out.println(numToEng(895));
         System.out.println(numToRus(0));
         System.out.println(numToRus(18));
-        System.out.println(numToRus(126));
-        System.out.println(numToRus(909));
+        System.out.println(numToRus(134));
+        System.out.println(numToRus(807));
         System.out.println();
 
         System.out.println("task 8");
@@ -75,56 +76,71 @@ public class Task5 {
 
     }
 
-
-    //1  Криптография через разницу кода символов
-    public static String encrypt(String line) {
-        // берём первый символ и добавляем в массив, размер которого равен длине строки
+    //    1. Пришло время отправлять и получать секретные сообщения.
+//    Создайте две функции, которые принимают строку и массив и возвращают
+//    закодированное или декодированное сообщение.
+//    Первая буква строки или первый элемент массива представляет собой символьный код
+//    этой буквы. Следующие элементы-это различия между символами: например, A +3 --> C
+//    или z -1 --> y.
+    public static int[] encrypt(String line) {
         int[] crypt = new int[line.length()];
         crypt[0] = line.charAt(0);
         // добавляем разность индексов с предыдущим символом в массив
         for (int i = 1; i < line.length(); i++) {
             crypt[i] = line.charAt(i) - line.charAt(i - 1);
         }
-        return Arrays.toString(crypt);
+        return crypt;
     }
 
-    public static StringBuilder decrypt(int... crypt) {
-        // создаём изменяемую строку, добавляем туда первый символ
+    public static String decrypt(int... crypt) {
         StringBuilder line = new StringBuilder(crypt.length);
         int sum = crypt[0];
         line.append((char) sum);
-        // для того, чтобы находить последующие, суммируем значения разности индексов символов, добавляя по индексу символы в строку
+        // суммируем значения разности индексов символов, добавляя по индексу символы в строку
         for (int i = 1; i < crypt.length; i++) {
             sum += crypt[i];
             line.append((char) sum);
         }
-        return line;
+        return line.toString();
     }
 
-
-    //2 шахматы
+    //    2. Создайте функцию, которая принимает имя шахматной фигуры, ее положение и
+//    целевую позицию. Функция должна возвращать true, если фигура может двигаться
+//    к цели, и false, если она не может этого сделать.
+//    Возможные входные данные - "пешка", "конь", "слон", "Ладья", "Ферзь"и " король".
     public static boolean canMove(String name, String a, String b) {
-        // разбиваем вторую и третью строку на координаты
         char[] x = a.toCharArray();
         char[] y = b.toCharArray();
-        // описываем все возможные ходы для каждой фигуры, по правилам шахмат. Не указан цвет фигуры, так что симметрия пешки
+        int x0 = x[0];
+        int y0 = y[0];
+        int x1 = x[1];
+        int y1 = y[1];
+
         return switch (name) {
-            case ("пешка") -> (Math.abs((int) x[0] - (int) y[0]) == 1) && (Math.abs((int) x[1] - (int) y[1]) == 1);
-            case ("конь") ->
-                    (Math.abs((int) x[0] - (int) y[0]) == 1) && (Math.abs((int) x[1] - (int) y[1]) == 2) || (Math.abs((int) x[0] - (int) y[0]) == 2) && (Math.abs((int) x[1] - (int) y[1]) == 1);
-            case ("слон") -> (Math.abs((int) x[0] - (int) y[0]) == Math.abs((int) x[1] - (int) y[1]));
-            case ("ладья") -> (Math.abs((int) x[0] - (int) y[0]) == 0) || (Math.abs((int) x[1] - (int) y[1]) == 0);
-            case ("ферзь") ->
-                    (Math.abs((int) x[0] - (int) y[0]) == Math.abs((int) x[1] - (int) y[1])) || (Math.abs((int) x[0] - (int) y[0]) == 0) || (Math.abs((int) x[1] - (int) y[1]) == 0);
-            case ("король") -> (Math.abs((int) x[0] - (int) y[0]) == 1) || (Math.abs((int) x[1] - (int) y[1]) == 1);
+            case ("пешка") -> (Math.abs(x0 - y0) == 1) && (Math.abs(x1 - y1) == 1);
+
+            case ("конь") -> (Math.abs(x0 - y0) == 1) && (Math.abs(x1 - y1) == 2) ||
+                    (Math.abs(x0 - y0) == 2) && (Math.abs(x1 - y1) == 1);
+
+            case ("слон") -> (Math.abs(x0 - y0) == Math.abs(x1 - y1));
+
+            case ("ладья") -> (Math.abs(x0 - y0) == 0) || (Math.abs(x1 - y1) == 0);
+
+            case ("ферзь") -> (Math.abs(x0 - y0) == Math.abs(x1 - y1)) ||
+                    (Math.abs(x0 - y0) == 0) || (Math.abs(x1 - y1) == 0);
+
+            case ("король") -> (Math.abs(x0 - y0) == 1) || (Math.abs(x1 - y1) == 1);
             default -> false;
         };
     }
 
-    //3 завершённость строки
+    //    3. Входная строка может быть завершена, если можно добавить дополнительные
+//    буквы, и никакие буквы не должны быть удалены, чтобы соответствовать слову.
+//    Кроме того, порядок букв во входной строке должен быть таким же, как и порядок
+//    букв в последнем слове.
+//    Создайте функцию, которая, учитывая входную строку, определяет, может ли слово быть
+//    завершено.
     public static boolean canComplete(String subline, String line) {
-        // Идём по строке, сравнивая поочерёдно с каждм символом субстроки.
-        // Если длинна субстроки равна счётку совпадений, true
         int count = 0;
         for (int i = 0; i < line.length(); i++) {
             if (line.charAt(i) == subline.charAt(count)) {
@@ -135,9 +151,10 @@ public class Task5 {
         return false;
     }
 
-    //4 сложение чисел массива, и перемножение цифр суммы, до 1 цифры
+    //    4. Создайте функцию, которая принимает числа в качестве аргументов, складывает их
+//    вместе и возвращает произведение цифр до тех пор, пока ответ не станет длиной
+//    всего в 1 цифру.
     public static int sumDigProd(int... massive) {
-        // как в предыдущем таске, только прежде суммируем все эллементы массива
         int number = 1;
         for (int i : massive) {
             number *= i;
@@ -153,76 +170,116 @@ public class Task5 {
         return number;
     }
 
-    //5 совпадение глассных
+
+    //    5. Напишите функцию, которая выбирает все слова, имеющие все те же гласные
+//    (в любом порядке и / или количестве), что и первое слово, включая первое слово.
     public static List<String> sameVowelGroup(String... lines) {
-        // Создаём результирующий список, массив уникальных эллементов
         List<String> result = new ArrayList<>();
         Set<Character> set = new HashSet<>();
         // по умолчанию вводим первое слово, вносим все его гласные в set, глассные находим через регулярку
         result.add(lines[0]);
-        String word1 = lines[0].replaceAll("[^aeiouy]", "");
-        for (int i = 0; i < word1.length(); i++) set.add(word1.charAt(i));
-        // идём по всем строка, начиная со второй, сравниваем глассные с теми, что есть в set
+        String vowelLettersFirstWord = lines[0].replaceAll("[^aeiouy]", "");
+        for (int i = 0; i < vowelLettersFirstWord.length(); i++) {
+            set.add(vowelLettersFirstWord.charAt(i));
+        }
+        // идём по всем строка, начиная со второй, сравниваем гласные с теми, что есть в set
         for (int i = 1; i < lines.length; i++) {
-            String word2 = lines[i].replaceAll("[^aeiouy]", "");
-            boolean k = true;
-            for (char c : word2.toCharArray())
+            String vowelLetters = lines[i].replaceAll("[^aeiouy]", "");
+            boolean flag = true;
+            for (char c : vowelLetters.toCharArray()) {
                 if (!set.contains(c)) {
-                    k = false;
+                    flag = false;
                     break;
                 }
-            if (k) result.add(lines[i]);
+            }
+            if (flag) {
+                result.add(lines[i]);
+            }
         }
         return result;
     }
 
-    //6 банковские карты
-    public static boolean validateCard(long number) {
-        // работаем с числами, возможно быстрее, чем со строками
-        if (Math.pow(10, 14) > number && Math.pow(10, 20) <= number) return false;
-        // выводим контрольную цифру из числа
-        int control = (int) number % 10;
-        number /= 10;
-        int sum = 0;
-        int k;
-        //идём по числу соблюдая условия.
-        for (int i = 1; Math.pow(10, i) <= number; i++) {
-            k = (int) (number % Math.pow(10, i) / Math.pow(10, i - 1) * (i % 2 + 1));
-            sum += k % 10 + k / 10;
+//    6. Создайте функцию, которая принимает число в качестве аргумента и возвращает
+//      true, если это число является действительным номером кредитной карты, а в
+//    противном случае-false.
+//    Номера кредитных карт должны быть длиной от 14 до 19 цифр и проходить тест Луна,
+//    описанный ниже:
+//            – Удалите последнюю цифру (это"контрольная цифра").
+//            – Переверните число.
+//            – Удвойте значение каждой цифры в нечетных позициях. Если удвоенное значение имеет
+//    более 1 цифры, сложите цифры вместе (например, 8 x 2 = 16 ➞ 1 + 6 = 7).
+//            – Добавьте все цифры.
+//– Вычтите последнюю цифру суммы (из шага 4) из 10. Результат должен быть равен
+//    контрольной цифре из Шага 1.
+
+    public static boolean validateCard(Long number) {
+        if (number < Math.pow(10, 14) || number >= Math.pow(10, 20)) {
+            return false;
         }
-        return 10 - sum % 10 == control;
+        // шаг 1 - находим контрольную сумму
+        int checkSum = (int) (number % 10);
+        String num = number.toString();
+        num = num.substring(0, num.length() - 1);
+        // шаг 2-3-4, идем по числу в обратном порядке, и сразу же складываем найденные цифры
+        int sum = 0;
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int doubleInt = Integer.parseInt(num.substring(i, i + 1));
+            if ((num.length() - i) % 2 == 1) {
+                doubleInt *= 2;
+                if (doubleInt > 9) {
+                    doubleInt = doubleInt % 10 + (doubleInt / 10) % 10;
+                }
+            }
+            sum += doubleInt;
+        }
+        // шаг 5 - сравниваем число с контрольной цифрой
+        return (10 - (sum % 10)) == checkSum;
     }
 
-    //7 число в литерацию
-    public static StringBuilder numToEng(int n) {
+    //    7. Напишите функцию, которая принимает положительное целое число от 0 до 999
+//    включительно и возвращает строковое представление этого целого числа,
+//    написанное на английском, руссском языке.
+    public static String numToEng(int n) {
         StringBuilder result = new StringBuilder();
-        String[] SUBTWENTY = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        if (n == 0) return result.append("zero").toString();
+        String[] SUBTWENTY = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
                 "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
         String[] DECADES = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-        if (n / 100 > 0) result.append(String.format("%s hundred ", SUBTWENTY[n / 100]));
-        if (n % 100 >= 20) result.append(String.format("%s ", DECADES[n % 100 / 10]));
-        if (n % 10 > 0 || n == 0) result.append(SUBTWENTY[n % 20]);
-        return result;
+        if (n / 100 != 0) {
+            result.append(String.format("%s hundred ", SUBTWENTY[n / 100]));
+        }
+        if (n % 100 >= 20) {
+            result.append(String.format("%s %s ", DECADES[n % 100 / 10 - 2], SUBTWENTY[n % 10]));
+        } else {
+            result.append(SUBTWENTY[n % 100]);
+        }
+        return result.toString();
     }
 
-    public static StringBuilder numToRus(int n) {
+    public static String numToRus(int n) {
         StringBuilder result = new StringBuilder();
-        String[] SUBTWENTY = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять",
+        if (n == 0) return result.append("ноль").toString();
+        String[] SUBTWENTY = {"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять",
                 "десять", "одинадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать",
                 "семнадцать", "восемнадцать", "девятнадцать"};
         String[] DECADES = {"двадцать", "тридцать", "сорок", "пятьдесят",
                 "шестьдесят", "семьдесят", "восемьдесят", "девяносто"};
-        String[] HUNDREDS = {"сто", "двесте", "триста", "четыреста",
-                "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"};
-        if (n / 100 > 0) result.append(String.format("%s ", HUNDREDS[n / 100 - 1]));
-        if (n % 100 >= 20) result.append(String.format("%s ", DECADES[n % 100 / 10 - 2]));
-        if (n % 10 > 0 || n == 0) result.append(SUBTWENTY[n % 20]);
-        return result;
+        String[] HUNDREDS = {"", "сто ", "двесте ", "триста ", "четыреста ",
+                "пятьсот ", "шестьсот ", "семьсот ", "восемьсот ", "девятьсот "};
+        result.append(HUNDREDS[n / 100]);
+        if (n % 100 >= 20) {
+            result.append(String.format("%s %s ", DECADES[n % 100 / 10 - 2], SUBTWENTY[n % 10]));
+        } else {
+            result.append(SUBTWENTY[n % 100]);
+        }
+        return result.toString();
     }
 
     //8 SHA-256
     public static String getSha256Hash(String Line) {
         try {
+//            MessageDigest представляет криптографическую хеш-функцию,
+//            которая может вычислять дайджест сообщения из двоичных данных.
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(Line.getBytes());
             StringBuilder hexString = new StringBuilder();
@@ -237,19 +294,35 @@ public class Task5 {
         }
     }
 
-    //9 Титул
+    //    9. Напишите функцию, которая принимает строку и возвращает строку с правильным
+//    регистром для заголовков символов в серии "Игра престолов".
+//    Слова and, the, of и in должны быть строчными. Все остальные слова должны иметь
+//    первый символ в верхнем регистре, а остальные-в Нижнем.
     public static String correctTitle(String text) {
-        StringBuilder right = new StringBuilder(text.toLowerCase());
-        //выставляем первый символ заглавным, если это буква
-        right.setCharAt(0, Character.toUpperCase(text.charAt(0)));
+        StringBuilder finalString = new StringBuilder(
+                text.toLowerCase());
+        //делаем первый символ прописным
+        finalString.setCharAt(0, Character.
+                toUpperCase(
+                        text.charAt(0)));
         //крутимся в цикле, и меняем буквы, перед которыми пробел на заглавные + требует проверки, что слово не является служебной частью речи
-        for (int i = 1; i < text.length(); i++)
-            if (Character.isSpaceChar(text.charAt(i - 1)) && !right.substring(i, i + 3).matches
-                    ("of |in |the|and")) right.setCharAt(i, Character.toUpperCase(text.charAt(i)));
-        return right.toString();
+        // в цикле меняем первую букву на заглавную в соответствие с требованиями
+        for (int i = 1; i < text.length(); i++) {
+            if (Character.isSpaceChar(text.charAt(i - 1)) && !finalString.substring(i, i + 3).matches("of |in |the|and")) {
+                finalString.setCharAt(i, Character.toUpperCase(text.charAt(i)));
+            }
+        }
+        return finalString.toString();
     }
 
-    //10 Гексогон
+    //    10. Как указано в онлайн-энциклопедии целочисленных последовательностей:
+//    Гексагональная решетка - это привычная двумерная решетка, в которой каждая точка
+//    имеет 6 соседей.Центрированное шестиугольное число - это центрированное фигурное число,
+//    представляющее шестиугольник с точкой в центре и всеми другими точками,
+//    окружающими центральную точку в шестиугольной решетке.
+//    Напишите функцию, которая принимает целое число n и возвращает "недопустимое", если
+//    n не является центрированным шестиугольным числом или его иллюстрацией в виде
+//    многострочной прямоугольной строки в противном случае.
     public static void hexLattice(int n) {
         // вычисляем модификатор, присваиваем его n
         for (int i = 1; n >= 1; i++) {
@@ -259,9 +332,10 @@ public class Task5 {
             }
             n -= i * 6;
         }
-        if (n < 0) System.out.println("Invalid");
-        // проход с модификатором, как можно обойтись без space?
-        // который бы контролировал отступ с динамичным n
+        if (n < 0) {
+            System.out.println("Invalid");
+        }
+
         int space = 1 + (n - 1) * 2;
         for (int i = space; i > 0; i--) {
             System.out.println(" ".repeat(Math.abs(space + 1 - n)) + "o ".repeat(n));
